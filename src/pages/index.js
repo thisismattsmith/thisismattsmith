66cd47f5ca26d77b56f0414b73cc0c7f92688bc5
thisismattsmith <hello@thisismattsmith.com>
2022-03-04 18:17:31 +1100

    fixed homepage tabs
    homepage now works like the accordion idea, except using tabs and radio buttons.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,19 @@ const HomePage = ({ data, location }) => {
         
         <LandingPage location={location}>
           <SEO title="Matt Smith" />
-          <h1 className="home-title"><Greeting />This is Matt Smith's website.       
-        I make stuff.
-        <br />
-        I write things.
+          <h1 className="home-title"><Greeting />This is <input type="radio" name="tabs" id="tab1"/> 
+<label for="tab1">Matt Smith</label>'s website.      
+<br />
+        I make <input type="radio" name="tabs" id="tab2"/> 
+<label for="tab2">stuff</label>.<br />
+I write <input type="radio" name="tabs" id="tab3" /><label for="tab3">things</label>.<br />
+  <div class="tab content1">
+    <p></p>
+    <p>I help L&D teams get better at what they do.</p>
+    <p>This includes fixing them completely.</p>
+</div> 
+<div class="tab content2">I do a whole bunch of stuff</div>
+<div class="tab content3">Tab3 Contents</div>
         
         <br />
         </h1>   
